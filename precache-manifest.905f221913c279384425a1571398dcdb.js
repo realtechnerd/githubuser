6028563bd64b57e27a331db16edaf6e4258a94e6
realtechnerd <send.nithil@gmail.com>
2020-09-19 20:55:34 -0400

    Updates

---
+++ precache-manifest.905f221913c279384425a1571398dcdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ae8bc8f0231ef93a0fc422ab222363",
+    "url": "/githubuser/index.html"
+  },
+  {
+    "revision": "702332ad63909137b145",
+    "url": "/githubuser/static/css/main.a6c287a2.chunk.css"
+  },
+  {
+    "revision": "15ae94c7886742ed2da1",
+    "url": "/githubuser/static/js/2.1c1029f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/githubuser/static/js/2.1c1029f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "702332ad63909137b145",
+    "url": "/githubuser/static/js/main.eea39865.chunk.js"
+  },
+  {
+    "revision": "061c46e5529c591c8347",
+    "url": "/githubuser/static/js/runtime-main.79d76295.js"
+  }
+]);
